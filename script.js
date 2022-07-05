623716e3278b1ebac8badef4dd0457d45c5d0e79
gabefando <gabe.fonseca.04@gmail.com>
2022-07-05 03:46:38 -0400

    added time in header built layout for timeblocks created function to save events to local storage

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
-var a = moment().toString();
+var a = moment('MM/DD/YYYY').toString();
 document.getElementById('currentDay').innerHTML = a;
 
-let task = {
-    task: 'test'
-};
-
-var task_ser = JSON.stringify(task);
-
-localStorage.setItem("task", task_ser);
-console.log(localStorage);
+var input1am = document.getElementById('input1am');
+var btn1am = document.getElementById("1ambtn");
+var del1am = document.getElementById("1amdel");
+check1am();
+btn1am.addEventListener("click", function(){
+    localStorage.setItem("task1am", input1am.value);
+    check1am;
+})
+del1am.addEventListener("click", function(){
+    localStorage.removeItem("task1am");
+    input1am.textContent = '';
+})
+function check1am() {
+    if (localStorage.getItem("task1am")) {
+        let x = localStorage.getItem("task1am");
+        input1am.textContent = x;
+    }
+    // if 1am > 12am && 1am < 2am change background color to .present
+}
